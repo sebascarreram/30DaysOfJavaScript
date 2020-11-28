15438e00cdd3360e4749c3545b21f2b351738e13
Sebastian Carrera <sebascarreram@hotmail.com>
2020-11-27 23:23:06 -0500

    Updated exercise lvl 1

--- day05/Exercise_lvl-1.js
+++ day05/Exercise_lvl-1.js
@@ -24,7 +24,7 @@ const webTechs = [
 
 console.log("~~~~~~~~~~~~~~~~~~");
 // Question 2
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numbers = [1, 2, 3, 4, 5, 8, 9, 10];
 console.log(numbers);
 
 // Question 3
@@ -37,11 +37,28 @@ console.log("~~~~~~~~~~~~~~~~~~");
 // Get the first item, the middle item and the last item of the array
 //
 const firstItem = numbers[0];
-const middleItem = numbers[Math.floor(numbersLen / 2)];
+
+let numbersMiddle;
+
+if (numbers.length === 2) {
+	numbersMiddle = "There is not item in medium";
+} else {
+	if (numbers.length % 2 === 0) {
+		numbersMiddle = numbers.slice(
+			Math.floor(numbersLen) / 2 - 1,
+			Math.floor(numbersLen) / 2 + 1
+		);
+	} else {
+		// oneItemMiddle = itCompanies.splice(notFloor, 1);
+		numbersMiddle = numbers[Math.floor(numbersLen / 2)];
+	}
+}
+
+// Last item in Array
 const lastItem = numbers[numbersLen - 1];
 
 console.log(
-	`First item: ${firstItem} || Middle item: ${middleItem} || Last item: ${lastItem}`
+	`First item: ${firstItem} || Middle item: ${numbersMiddle} || Last item: ${lastItem}`
 );
 
 console.log("~~~~~~~~~~~~~~~~~~");
@@ -63,6 +80,7 @@ const mixedDataTypes = [
 
 const mixedLeng = mixedDataTypes.length;
 
+console.log(mixedDataTypes);
 const messageOne = `This array has: ${mixedLeng}`;
 const messageTwo = `This array size sould be greater than 5, Try again later`;
 
@@ -88,16 +106,13 @@ const itCompanies = [
 console.log(itCompanies);
 
 // The size of array Companies
-const companiesLen = itCompanies.length;
+let companiesLen = itCompanies.length;
 console.log(companiesLen);
 
 const firstCompany = itCompanies[0];
-const middleCompany = itCompanies[Math.floor(companiesLen / 2)];
 const lastCompany = itCompanies[companiesLen - 1];
 
-console.log(
-	`First company: ${firstCompany} || Middle company: ${middleCompany} || Last company: ${lastCompany}`
-);
+console.log(`First company: ${firstCompany} || Last company: ${lastCompany}`);
 
 // Print out each company
 console.log(`1. ${itCompanies[0]}`);
@@ -166,6 +181,7 @@ console.log(reverseCompany);
 console.log("~~~~~~~~~~~~~~~");
 
 // Slice out the first 3 companies from the array
+const middleCompany = itCompanies[Math.floor(companiesLen / 2)];
 //
 //console.log(itCompanies)
 
@@ -173,8 +189,8 @@ const firstThree = itCompanies.slice(0, 3);
 console.log("The first 3 companies");
 console.log(firstThree);
 
+console.log("##");
 // Slice out the last 3 companies from the array
-//
 
 const coLen = itCompanies.length;
 const lastThree = itCompanies.slice(coLen - 3, coLen);
@@ -182,10 +198,31 @@ const lastThree = itCompanies.slice(coLen - 3, coLen);
 console.log("The last 3 companies");
 console.log(lastThree);
 
-// Slice out the middle company
+console.log("##");
+// Slice out the middle IT company or companies from the array
 const notFloor = Math.floor(coLen / 2);
-console.log("The middle company");
-console.log(itCompanies[notFloor]);
+
+if (companiesLen === 2) {
+	console.log("There is not middle");
+} else {
+	let oneItemMiddle;
+	let twoItemsMiddle;
+	if (itCompanies.length % 2 === 0) {
+		// 2 item in medium
+		twoItemsMiddle = itCompanies.slice(
+			Math.floor(itCompanies.length) / 2 - 1,
+			Math.floor(itCompanies.length) / 2 + 1
+		);
+		console.log("2 items medium in array");
+		console.log(twoItemsMiddle);
+	} else {
+		// companiesLen % 2 === 1;
+		//
+		oneItemMiddle = itCompanies.splice(notFloor, 1);
+		console.log("1 item medium in array");
+		console.log(oneItemMiddle);
+	}
+}
 
 console.log("~~~~~~~~~~~~~~~");
 
@@ -196,28 +233,52 @@ console.log(itCompanies);
 
 const deleteFirstItem = itCompanies.shift();
 
-console.log("First array is Removed");
+console.log(`First removed -> ${deleteFirstItem}`);
 console.log(itCompanies);
 
 console.log("~~~~~~~~~~~~~~~");
 // Remove the middle IT company or companies from the array
 //
 
-const deleteMiddle = itCompanies.splice(notFloor, 1);
-console.log("Removed middle array");
-console.log(deleteMiddle);
-
-//
-//
-console.log("~~~~~~~~~~~~~~~");
-// Remove the last IT company from the array
-console.log("Array original");
+console.log("###");
+console.log("Array again here");
 console.log(itCompanies);
+console.log(`This array has ${itCompanies.length} lengths`);
+console.log("###");
+
+if (companiesLen === 2) {
+	console.log("There is not middle");
+} else {
+	let deleteMiddle;
+	if (itCompanies.length % 2 === 0) {
+		// remove 1 item in medium
+
+		deleteMiddle = itCompanies.splice(
+			Math.floor(itCompanies.length) / 2 - 1,
+			2
+		);
+		console.log("~~~~~~~~~~~~~~~");
+		console.log(`Removed 2 items medium in array`);
+		console.log(`Removed -> ${deleteMiddle}`);
+		console.log("Array now");
+		console.log(itCompanies);
+	} else {
+		console.log("~~~~~~~~~~~~~~~");
+		// companiesLen % 2 === 0;
+		//
+		deleteMiddle = itCompanies.splice(notFloor, 1);
+		console.log("Removed 1 item medium in array");
+		console.log(`Removed -> ${deleteMiddle}`);
+		console.log("Arrow now");
+		console.log(itCompanies);
+	}
+}
 
 console.log("~~~~~~~~~~~~~~~");
 const deleteLastItem = itCompanies.pop();
 
-console.log("Last array is Removed");
+console.log(`Removed last item -> ${deleteLastItem}`);
+console.log("Array now");
 console.log(itCompanies);
 
 console.log("~~~~~~~~~~~~~~~");
