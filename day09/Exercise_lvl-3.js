8a9d0c7077ae31b4969a821d2ef52c9a35760b98
Sebastian Carrera <sebascarreram@hotmail.com>
2021-01-13 22:42:59 -0500

    Well, i am here !

---
+++ day09/Exercise_lvl-3.js
@@ -0,0 +1,280 @@
+const { countriesData } = require("./countries_data");
+
+const countries = [
+	{
+		country: "United State Of America",
+		capital: "Washington, D.C.",
+		population: "332050976"
+	},
+	{ country: "Greenland", capital: "Nuuk", population: "56823" },
+	{ country: "Colombia", capital: "Bogota", population: "51178000" },
+	{ country: "Canada", capital: "Ottawa", population: "37921695" },
+	{ country: "Vietnam", capital: "Hahoi", population: "97338579" },
+	{ country: "Spain", capital: "Madrid", population: "46754778" }
+];
+
+const countriesLanguages = [
+	// count = millones
+	{ country: "English", count: 1268 },
+	{ country: "French", count: 277 },
+	{ country: "Arabic", count: 274 },
+	{ country: "Spanish", count: 538 },
+	{ country: "Japanese", count: 126 },
+	{ country: "Marathi", count: 95 },
+	{ country: "Hindi", count: 637 },
+	{ country: "Russian", count: 258 },
+	{ country: "Bengali", count: 265 },
+	{ country: "Portuguese", count: 252 },
+	{ country: "Dutch", count: 24 },
+	{ country: "German", count: 132 },
+	{ country: "Mandarin Chinese", count: 1120 },
+	{ country: "Swahili", count: 99 }
+];
+console.log("List of countries");
+console.log(countries);
+console.log("~~~~~~~~~~~~~~~");
+// Question 1
+// Use the countries information, in the data folder. Sort countries by name, by capital, by population
+//
+// Copy array
+const copyCountries = [...countries];
+
+const infoCountries = elements => {
+	// sort by country name
+	const countriesAsce = elements.sort(function (a, b) {
+		let nameA = a.country.toUpperCase(); // ignore upper and lowercase
+		let nameB = b.country.toUpperCase(); // ignore upper and lowercase
+
+		// a is less than b by some ordering criterion
+		if (nameA < nameB) {
+			return -1;
+		}
+		// a is greater than b by the ordering criterion
+		if (nameA > nameB) {
+			return 1;
+		}
+
+		// a must be equal to b
+		return 0;
+	});
+	console.log(countriesAsce);
+};
+infoCountries(copyCountries);
+console.log("~~~~~~~~~~~~~~~");
+// Question 2
+// *** Find the 10 most spoken languages:
+//
+// Your output should look like this
+//
+// console.log(mostSpokenLanguages(countries, 10))
+// [
+//  { country: 'English', count: 91 },
+//  { country: 'French', count: 45 },
+//  { country: 'Arabic', count: 25 },
+//  { country: 'Spanish', count: 24 },
+//  { country: 'Russian', count: 9 },
+//  { country: 'Portuguese', count: 9 },
+//  { country: 'Dutch', count: 8 },
+//  { country: 'German', count: 7 },
+//  { country: 'Chinese', count: 5 },
+//  { country: 'Swahili', count: 4 }
+// ]
+
+// Your output should look like this
+// console.log(mostSpokenLanguages(countries, 3))
+// [
+// 	{ country: 'English', count: 91 },
+// 	{ country: 'French', count: 45 },
+// 	{ country: 'Arabic', count: 25 },
+// ]
+//
+const copyLanguage = [...countriesLanguages];
+
+const mostSpokenLanguages = (elements, nbr) => {
+	const countryLen = elements.length;
+	//
+	if (nbr >= countryLen) {
+		console.log(`Try only these numbers 0 - ${countryLen}`);
+		return;
+	}
+	// Sort by count
+	elements.sort((a, b) => b.count - a.count);
+
+	//
+	const nbrArray = elements.filter((elem, index) => {
+		if (index < nbr) {
+			return elem;
+		}
+	});
+	console.log(`the ${nbr} most spoken languages`);
+	console.log(nbrArray);
+};
+mostSpokenLanguages(copyLanguage, 6);
+console.log("~~~~~~~~~~~~~~~");
+// Question 3
+// *** Use countries_data.js file create a function which create the ten most populated countries
+//
+// [
+//  {country: 'China', population: 1377422166},
+//  {country: 'India', population: 1295210000},
+//  {country: 'United States of America', population: 323947000},
+//  {country: 'Indonesia', population: 258705000},
+//  {country: 'Brazil', population: 206135893},
+//  {country: 'Pakistan', population: 194125062},
+//  {country: 'Nigeria', population: 186988000},
+//  {country: 'Bangladesh', population: 161006790},
+//  {country: 'Russian Federation', population: 146599183},
+//  {country: 'Japan', population: 126960000}
+// ]
+//
+// console.log(mostPopulatedCountries(countries, 3))
+// [
+//  {country: 'China', population: 1377422166},
+//  {country: 'India', population: 1295210000},
+//  {country: 'United States of America', population: 323947000}
+// ]
+//
+//
+// Copy array because sort() method will change the original SO i don't want to change
+const countryCopy = [...countriesData];
+
+const mostPopulatedCountries = (elements, len) => {
+	// Sort by population
+	elements.sort((a, b) => b.population - a.population);
+
+	// print until n
+	const print = elements.filter((elem, index) => {
+		if (index < len) {
+			return elem;
+		}
+	});
+	console.log(`The ${len} most spoken languages`);
+	return print;
+};
+
+console.log(mostPopulatedCountries(countryCopy, 3));
+console.log("~~~~~~~~~~~~~~~");
+// Question 4
+// *** Try to develop a program which calculate measure of central tendency of a sample(mean, median, mode) and
+// measure of variability(range, variance, standard deviation).
+// In addition to those measures find the min, max, count, percentile, and frequency distribution of the sample.
+// You can create an object called statistics and create all the functions which do statistical calculations as
+// method for the statistics object. Check the output below.
+//
+
+const ages = [
+	31,
+	26,
+	34,
+	37,
+	27,
+	26,
+	32,
+	32,
+	26,
+	27,
+	27,
+	24,
+	32,
+	33,
+	27,
+	25,
+	26,
+	38,
+	37,
+	31,
+	34,
+	24,
+	33,
+	29,
+	26
+];
+//
+// console.log('Count:', statistics.count()) // 25
+// console.log('Sum: ', statistics.sum()) // 744
+// console.log('Min: ', statistics.min()) // 24
+// console.log('Max: ', statistics.max()) // 38
+// console.log('Range: ', statistics.range() // 14
+// console.log('Mean: ', statistics.mean()) // 30
+// console.log('Median: ',statistics.median()) // 29
+// console.log('Mode: ', statistics.mode()) // {'mode': 26, 'count': 5}
+// console.log('Variance: ',statistics.var()) // 17.5
+// console.log('Standard Deviation: ', statistics.std()) // 4.2
+// console.log('Frequency Distribution: ',statistics.freqDist())
+//	[
+//  	(20.0, 26),
+//  	(16.0, 27),
+//  	(12.0, 32),
+//  	(8.0, 37),
+//  	(8.0, 34),
+//  	(8.0, 33),
+//  	(8.0, 31),
+//  	(8.0, 24),
+//  	(4.0, 38),
+//  	(4.0, 29),
+//		(4.0, 25)
+//	]
+//
+const copyNbr = [...ages];
+
+const statistics = {
+	count: function () {
+		let nbr = 0;
+		ages.forEach(() => nbr++);
+		// or const agesLen = ages.length;
+		return nbr;
+	},
+	sum: function () {
+		const sum = ages.reduce((acc, curr) => acc + curr);
+		return sum;
+	},
+	min: function () {
+		copyNbr.sort((a, b) => a - b);
+		return copyNbr[0];
+	},
+	max: function () {
+		copyNbr.sort((a, b) => b - a);
+		return copyNbr[0];
+	},
+	range: function () {
+		return this.max() - this.min();
+	},
+	mean: function () {
+		return Math.round(this.sum() / this.count());
+	},
+	median: function () {
+		return copyNbr[Math.floor(this.count() / 2)];
+	},
+	mode: function () {
+		// "Nope yet"
+		return "See you tomorrow";
+	},
+	variance: function () {
+		const ac = copyNbr.map(elem => (elem - this.mean()) ** 2);
+		const sum = ac.reduce((acc, curr) => acc + curr, 0);
+		const n = this.count() - 1;
+		const s = sum / n;
+		return s.toFixed(2);
+	},
+	std: function () {
+		return Math.sqrt(this.variance()).toFixed(2);
+	},
+	freqDist: function () {
+		// "Nope yet"
+		return "See you tomorrow";
+	}
+};
+
+console.log("Count: ", statistics.count());
+console.log("Sum: ", statistics.sum());
+console.log("Min: ", statistics.min());
+console.log("Max: ", statistics.max());
+console.log("Range: ", statistics.range());
+console.log("Mean: ", statistics.mean());
+console.log("Median: ", statistics.median());
+console.log("Mode: ", statistics.mode());
+console.log("Variance: ", statistics.variance());
+console.log("Standard Deviation: ", statistics.std());
+console.log("Standard Deviation: ", statistics.std());
+console.log("Frequency Distribution: ", statistics.freqDist());
+console.log("~~~~~~~~~~~~~~~");
